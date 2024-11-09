63a0d8e22501a6ee16f34183ca2f44eca0740449
Doupix <doupix@dpx.net>
2024-11-08 22:38:19 -0500

    Modif nom storage

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
   }
   backend "azurerm" {
     resource_group_name  = "VM_Builder"                    # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-    storage_account_name = "storage-hzvtevxyevcxyezjvz"    # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    storage_account_name = "storagehzvtevxyevcxyezjvz"     # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "state"                         # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "Web_cluster.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
